10c15a408c900acbfa6fd56b31a74c51477491ff
Daniyal Ahmad <daniyalahmad.se@gmail.com>
2023-09-13 13:13:15 +0500

    Generalized SheetsApiProvider Get

--- pages/assets/js/services/blogs_manager.js
+++ pages/assets/js/services/blogs_manager.js
@@ -5,14 +5,58 @@ class BlogsManager {
         this.sheetsApiProvider = sheetsApiProvider;
     }
 
-    async #getBlogs(skip, count) {
-        try {
-            let blogs = await this.sheetsApiProvider.get('Blogs');
-            blogs = ArrayUtils.customSort(blogs).slice(skip, count == 0 ? blogs.length : count);
-            return blogs;
-        } catch (error) {
-            console.error('Error getting blogs:', error);
+    #sortBlogs(inputArray) {
+        function sortByIsFeaturedAndDatePosted(a, b) {
+            // Compare by IsFeatured first
+            if (a.IsFeatured && !b.IsFeatured) {
+                return -1;
+            }
+            if (!a.IsFeatured && b.IsFeatured) {
+                return 1;
+            }
+
+            // If both have the same IsFeatured status, compare by DatePosted
+            const dateA = new Date(a.DatePosted);
+            const dateB = new Date(b.DatePosted);
+
+            if (dateA > dateB) {
+                return -1;
+            }
+            if (dateA < dateB) {
+                return 1;
+            }
+
+            return 0;
         }
+
+        inputArray.sort(sortByIsFeaturedAndDatePosted);
+        return inputArray;
+    }
+
+    #convertBlogsArrayToObjects(dataArray) {
+        const [headerRow, ...rows] = dataArray; // Extract header row and rest of the rows
+        const headerKeys = headerRow.map(String); // Ensure header keys are strings
+
+        const objectsArray = rows.map(row => {
+            const obj = {};
+            row.forEach((value, index) => {
+                // Check data types and convert values accordingly
+                if (headerKeys[index] === "Tags") {
+                    // Split the comma-separated string into an array of words
+                    obj[headerKeys[index]] = value.split(',').map(tag => tag.trim());
+                } else if (value === "TRUE" || value === "FALSE") {
+                    obj[headerKeys[index]] = value === "TRUE"; // Convert to boolean
+                } else if (!isNaN(value) && !isNaN(parseFloat(value))) {
+                    const floatValue = parseFloat(value);
+                    obj[headerKeys[index]] = Number.isInteger(floatValue) ? parseInt(value) : floatValue; // Convert to integer or float
+                } else {
+                    obj[headerKeys[index]] = value; // Keep as string
+                }
+            });
+            return obj;
+        });
+
+        return objectsArray;
     }
 
     #createBlogCard(blog, outerDivClasses, innerDivClasses) {
@@ -138,6 +182,20 @@ class BlogsManager {
         containerDiv.appendChild(singleArticleDiv);
 
         return containerDiv;
+    }
+
+    async #getBlogs(skip, count) {
+        try {
+            let blogs = await this.sheetsApiProvider.get('Blogs');
+            let blogTags = await this.sheetsApiProvider.get('BlogTags');
+
+            blogs = this.#sortBlogs(
+                this.#convertBlogsArrayToObjects(blogs)
+            ).slice(skip, count == 0 ? blogs.length : count);
+            return blogs;
+        } catch (error) {
+            console.error('Error getting blogs:', error);
+        }
     }
 
     async addBlogCardsToContainer(containerId, count, skip, outerDivClasses, innerDivClasses) {
--- pages/assets/js/utilities/array_utils.js
+++ pages/assets/js/utilities/array_utils.js
@@ -1,32 +1,4 @@
 class ArrayUtils {
-    static customSort(inputArray) {
-        function sortByIsFeaturedAndDatePosted(a, b) {
-            // Compare by IsFeatured first
-            if (a.IsFeatured && !b.IsFeatured) {
-                return -1;
-            }
-            if (!a.IsFeatured && b.IsFeatured) {
-                return 1;
-            }
-
-            // If both have the same IsFeatured status, compare by DatePosted
-            const dateA = new Date(a.DatePosted);
-            const dateB = new Date(b.DatePosted);
-
-            if (dateA > dateB) {
-                return -1;
-            }
-            if (dateA < dateB) {
-                return 1;
-            }
-
-            return 0;
-        }
-
-        inputArray.sort(sortByIsFeaturedAndDatePosted);
-        return inputArray;
-    }
-
     static convertArrayToObjects(dataArray) {
         const [headerRow, ...rows] = dataArray; // Extract header row and rest of the rows
         const headerKeys = headerRow.map(String); // Ensure header keys are strings
@@ -35,10 +7,7 @@ class ArrayUtils {
             const obj = {};
             row.forEach((value, index) => {
                 // Check data types and convert values accordingly
-                if (headerKeys[index] === "Tags") {
-                    // Split the comma-separated string into an array of words
-                    obj[headerKeys[index]] = value.split(',').map(tag => tag.trim());
-                } else if (value === "TRUE" || value === "FALSE") {
+                if (value === "TRUE" || value === "FALSE") {
                     obj[headerKeys[index]] = value === "TRUE"; // Convert to boolean
                 } else if (!isNaN(value) && !isNaN(parseFloat(value))) {
                     const floatValue = parseFloat(value);
